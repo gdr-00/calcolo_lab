67d8ab90cfc9259a663c491f4907ff6c87e04c73
gdr-00 <elborghino@gmail.com>
2023-05-12 19:24:49 +0200

    added ex2 casa lez3

--- lab_3/ex2_casa_lez3.m
+++ lab_3/ex2_casa_lez3.m
@@ -5,4 +5,4 @@ y2=(1/2)*(sin(x/2)^2)/(x/2)^2
 
 %in questo caso sono equivalenti ma per evitare errori di calcolo dovuti ad
 %arrotondamenti per la sottrazione in y1 è meglio utilizzare y2 che è
-%stabile    
+%stabile
